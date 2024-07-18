b433f4c773cc8c4df8a9e9497242ceff2c66eb00
Abhi-tech-09 <abhishekalien09@gmail.com>
2024-07-18 14:49:07 +0530

    Hackathon begins

---
+++ src/Route.tsx
@@ -0,0 +1,65 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Stories from "./pages/Stories";
+import { useAuth } from "./contexts/AuthContextProvider";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { get } from "./firebase/firebase";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    // children: {
+    //   // path: "/careta"
+    // }
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/stories",
+    element: <Stories />,
+  },
+]);
+const Route = () => {
+  const { setUser } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const auth = getAuth();
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          const uid = user.uid;
+          console.log(user);
+          if (user.displayName === null) {
+            try {
+              const userFromDB = await get("users", user.uid);
+              console.log(userFromDB.data());
+              if (userFromDB.exists()) {
+                setUser({ name: userFromDB.data().name, uid: user.uid });
+              }
+            } catch (e: any) {
+              console.log("Error getting name from db", e);
+            }
+          } else setUser({ name: user.displayName, uid: uid });
+        } else {
+          console.log("User hi nahi hai");
+          // User is signed out
+          // ...
+        }
+      });
+    })();
+  }, []);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Route;
